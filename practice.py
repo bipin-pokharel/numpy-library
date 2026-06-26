ed61c5652d2db22e7a77b60352e3e2b3f488254e
bipin pokharel <pokharelbipin850@gmail.com>
2026-06-26 23:51:16 +0545

    in dimensional

--- practice.py
+++ practice.py
@@ -5,3 +5,7 @@ my_array = np.array([10, 20, 30, 40, 50])
 print("NumPy Version:", np.__version__)
 print("Our Array:", my_array)
 print("Array Mean:", np.mean(my_array))
+
+
+b = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
+print(b)
